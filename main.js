d79f7dbc0e6427502755e6b01e1c840a689b6487
PearceB <Bobtarznit@gmail.com>
2014-01-28 12:57:39 -0600

    End of Class
    Added background coding

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.onload = function() {
 	var game = new Core(320, 320);
 	game.fps = 32;
 	game.preload('http://img153.imageshack.us/img153/442/previewpg6.jpg',
-				  'http://enchantjs.com/assets/images/map0.gif');
+				 'http://enchantjs.com/assets/images/map0.gif');
 	
 // Load the game state as played
 	game.onload = function() {
@@ -16,4 +16,11 @@ window.onload = function() {
 		var bg = new Sprite(320, 320);
 		var map = game.assets['http://enchantjs.com/assets/images/map0.gif'];
 		var image = new Surface(320, 320);
-		for (var i = 0; i < 320; i+=
+		for (var i = 0; i < 320; i += 16) {
+			for (var j = 0; j < 320; j += 16) {
+				image.draw(map, 160, 260, 16, 16, j, i, 16, 16);
+			}
+		}
+		
+		bg.image = image;
+		game.rootScene.addChild(bg);
