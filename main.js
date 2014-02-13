8940d7913d5866209df47224f44d3c5223ffd612
PearceB <Bobtarznit@gmail.com>
2014-02-13 12:58:56 -0600

    End of Class
    Moved the "paddle" but have major sprite/pixeling issues

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Variables for the game
 var LEFT = 0;
 var RIGHT = 1;
+var UP = 2;
+var DOWN = 3;
 
 // Execute the Enchant library
 enchant();
@@ -28,19 +30,64 @@ window.onload = function() {
 		bg.image = image;
 		game.rootScene.addChild(bg);
 		
-		var paddle = new Sprite(24, 24);
+		var paddle = new Sprite(32, 32);
 		paddle.image = game.assets['http://img153.imageshack.us/img153/442/previewpg6.jpg'];
 		paddle.x = 160 - 16;
 		paddle.y = 160 - 16;
-		paddle.frame = 11;
+		paddle.frame = 3645;
 		
 		paddle.toX = paddle.x;
-		paddle.dir = RIGHT;
+		paddle.toY = paddle.y;
+		paddle.dir = DOWN;
+		paddle.anim = [3645];
 		
 		game.rootScene.addChild(paddle);
+		
+		paddle.addEventListener(Event.ENTER_FRAME, function() {
+            if (paddle.y > paddle.toY) {
+                paddle.dir = UP;
+                if (Math.abs(paddle.y - paddle.toY) < 3) {
+                    paddle.y = paddle.toY;
+                } else {
+                    paddle.y -= 3;
+                }
+            } else if (paddle.y < paddle.toY) {
+                paddle.dir = DOWN;
+                if (Math.abs(paddle.y - paddle.toY) < 3) {
+                    paddle.y = paddle.toY;
+                } else {
+                    paddle.y += 3;
+                }
+            }
+            if (paddle.x > paddle.toX) {
+                paddle.dir = LEFT;
+                if (Math.abs(paddle.x - paddle.toX) < 3) {
+                    paddle.x = paddle.toX;
+                } else {
+                    paddle.x -= 3;
+                }
+            } else if (paddle.x < paddle.toX) {
+                paddle.dir = RIGHT;
+                if (Math.abs(paddle.x - paddle.toX) < 3) {
+                    paddle.x = paddle.toX;
+                } else {
+                    paddle.x += 3;
+                }
+            }
+            
+            if (paddle.x == paddle.toX && paddle.y == paddle.toY)
+                paddle.age = 1;
+            paddle.frame = paddle.anim[paddle.dir *4 + (paddle.age % 4)];
+        });
 	
-	bg.addEventListener(Event.TOUCH_START, function(e) {
-		paddle.toX = e.x - 16;
+		bg.addEventListener(Event.TOUCH_START, function(e) {
+			paddle.toX = e.x - 16;
+			paddle.toY = e.y - 16;
+		});
+			
+		bg.addEventListener(Event.TOUCH_MOVE, function(e) {
+			paddle.toX = e.x - 16;
+			paddle.toY = e.y - 16;
 		});
 	};
 	game.start();
